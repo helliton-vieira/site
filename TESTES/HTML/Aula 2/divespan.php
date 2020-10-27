5e17ef8256bef9ebf0f69460eb8a2121c2931bea
helliton-vieira <73505986+helliton-vieira@users.noreply.github.com>
2020-10-26 21:32:15 -0300

    Add files via upload

---
+++ TESTES/HTML/Aula 2/divespan.php
@@ -0,0 +1,11 @@
+<html>
+	<head>
+	<title>SEJAM BEM VINDOS</title>
+	<link href="style1.css" rel="stylesheet" />
+	</head>
+	<body>
+	
+		<div align="center">LOJA TUMARE</div>
+
+	</body>
+</html>
---
+++ TESTES/HTML/Aula 2/introduzindocss.php
@@ -0,0 +1,15 @@
+
+<html>
+	<head>
+		<title>Seja bem vindo(a)!</title>	
+		<link href="style.css" rel="stylesheet" />
+	</head>		
+	<body class="corpo1">
+	<h1>Titulo da Página</h1>
+	Este é o testo da minha página</br>
+	<img class="teste" src="../../imagens/placa de video.jpg" alt="placa de video" title="não clica na foto" />
+	<img id="titulo" src="../../imagens/placa de video.jpg" alt="placa de video" title="não clica na foto" />
+	<p>_____________________________________________________________________________________</p>
+	<div>LOJA</div>
+	</body>
+</html>
